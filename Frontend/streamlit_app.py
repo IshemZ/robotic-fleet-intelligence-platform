dc4538ff86edcbf714762dc158fcf07d4ee78803
IshemZ <ishem.zerzour@gmail.com>
2025-05-17 17:21:08 +0200

    formating PEP8

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
-from datetime import datetime
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -13,30 +9,36 @@ load_dotenv()
 
 URI = os.getenv("URI_MONGODB")
 
-#Connexion MongoDB
+# Connexion MongoDB
 client = MongoClient(URI)
 db = client["opensky_data"]
 collection = db["fleet_intelligence"]
 
-#Récupération Data et converison dataframe
+# Récupération Data et converison dataframe
 data = list(collection.find({}))
 df = pd.DataFrame(data)
 df = df.drop(columns=["_id"])
-df['latitude'] = df['latitude'].astype(float)
-df['longitude'] = df['longitude'].astype(float)
-df = df[df['latitude'].notnull() & df['longitude'].notnull()]
+df["latitude"] = df["latitude"].astype(float)
+df["longitude"] = df["longitude"].astype(float)
+df = df[df["latitude"].notnull() & df["longitude"].notnull()]
 
 
-#SideBar
+# SideBar
 st.sidebar.title("Menu")
-vue_global = st.sidebar.segmented_control(options=['Décollage','Atterissage'],label="Etat de l'avion", selection_mode='single')
+vue_global = st.sidebar.segmented_control(
+    options=["Décollage", "Atterissage"],
+    label="Etat de l'avion",
+    selection_mode="single",
+)
 
-choix = st.sidebar.selectbox("choisi un pays", ["France","Allemagne"])
+choix = st.sidebar.selectbox("choisi un pays", ["France", "Allemagne"])
 
-Projet1 =st.sidebar.button("Vue 1", use_container_width=True, disabled=False, type="secondary")
+Projet1 = st.sidebar.button(
+    "Vue 1", use_container_width=True, disabled=False, type="secondary"
+)
 Projet2 = st.sidebar.button("Vue 2", use_container_width=True)
 
-#Map
-map_data = df[['latitude',"longitude"]]
+# Map
+map_data = df[["latitude", "longitude"]]
 st.map(map_data, use_container_width=True, color="#ffaa00")
 df
